4ed24ce3f306a344b1608d6887b7996fbe6d0f0f
Element Green <jgreen@users.sourceforge.net>
2013-10-17 21:20:56 -0600

    Removed a lot of unused DLS to SF2 conversion code for now.

--- libinstpatch/libinstpatch/IpatchConvert_SF2.h
+++ libinstpatch/libinstpatch/IpatchConvert_SF2.h
@@ -39,34 +39,6 @@ typedef IpatchConverterClass IpatchConverterFileToSF2Class;
 typedef IpatchConverter IpatchConverterFileToSF2Sample;
 typedef IpatchConverterClass IpatchConverterFileToSF2SampleClass;
 
-#if 0
-typedef IpatchConverter IpatchConverterSF2ToDLS2;
-typedef IpatchConverterClass IpatchConverterSF2ToDLS2Class;
-typedef IpatchConverter IpatchConverterDLS2ToSF2;
-typedef IpatchConverterClass IpatchConverterDLS2ToSF2Class;
-typedef IpatchConverter IpatchConverterSF2PresetToDLS2Inst;
-typedef IpatchConverterClass IpatchConverterSF2PresetToDLS2InstClass;
-typedef IpatchConverter IpatchConverterDLS2InstToSF2Preset;
-typedef IpatchConverterClass IpatchConverterDLS2InstToSF2PresetClass;
-typedef IpatchConverter IpatchConverterSF2InstToDLS2Inst;
-typedef IpatchConverterClass IpatchConverterSF2InstToDLS2InstClass;
-typedef IpatchConverter IpatchConverterDLS2InstToSF2Inst;
-typedef IpatchConverterClass IpatchConverterDLS2InstToSF2InstClass;
-typedef IpatchConverter IpatchConverterSF2PZoneToDLS2Region;
-typedef IpatchConverterClass IpatchConverterSF2PZoneToDLS2RegionClass;
-typedef IpatchConverter IpatchConverterDLS2RegionToSF2PZone;
-typedef IpatchConverterClass IpatchConverterDLS2RegionToSF2PZoneClass;
-typedef IpatchConverter IpatchConverterSF2IZoneToDLS2Region;
-typedef IpatchConverterClass IpatchConverterSF2IZoneToDLS2RegionClass;
-typedef IpatchConverter IpatchConverterDLS2RegionToSF2IZone;
-typedef IpatchConverterClass IpatchConverterDLS2RegionToSF2IZoneClass;
-typedef IpatchConverter IpatchConverterSF2SampleToDLS2Sample;
-typedef IpatchConverterClass IpatchConverterSF2SampleToDLS2SampleClass;
-typedef IpatchConverter IpatchConverterDLS2SampleToSF2Sample;
-typedef IpatchConverterClass IpatchConverterDLS2SampleToSF2SampleClass;
-#endif
-
-
 #define IPATCH_TYPE_CONVERTER_SF2_TO_FILE \
   (ipatch_converter_sf2_to_file_get_type ())
 #define IPATCH_TYPE_CONVERTER_FILE_TO_SF2 \
@@ -74,52 +46,9 @@ typedef IpatchConverterClass IpatchConverterDLS2SampleToSF2SampleClass;
 #define IPATCH_TYPE_CONVERTER_FILE_TO_SF2_SAMPLE \
   (ipatch_converter_file_to_sf2_sample_get_type ())
 
-#if 0
-#define IPATCH_TYPE_CONVERTER_SF2_TO_DLS2 \
-  (ipatch_converter_sf2_to_dls2_get_type ())
-#define IPATCH_TYPE_CONVERTER_DLS2_TO_SF2 \
-  (ipatch_converter_dls2_to_sf2_get_type ())
-#define IPATCH_TYPE_CONVERTER_SF2_PRESET_TO_DLS2_INST \
-  (ipatch_converter_sf2_preset_to_dls2_inst_get_type ())
-#define IPATCH_TYPE_CONVERTER_DLS2_INST_TO_SF2_PRESET \
-  (ipatch_converter_dls2_inst_to_sf2_preset_get_type ())
-#define IPATCH_TYPE_CONVERTER_SF2_INST_TO_DLS2_INST \
-  (ipatch_converter_sf2_inst_to_dls2_inst_get_type ())
-#define IPATCH_TYPE_CONVERTER_DLS2_INST_TO_SF2_INST \
-  (ipatch_converter_dls2_inst_to_sf2_inst_get_type ())
-#define IPATCH_TYPE_CONVERTER_SF2_PZONE_TO_DLS2_REGION \
-  (ipatch_converter_sf2_pzone_to_dls2_region_get_type ())
-#define IPATCH_TYPE_CONVERTER_DLS2_REGION_TO_SF2_PZONE \
-  (ipatch_converter_dls2_region_to_sf2_pzone_get_type ())
-#define IPATCH_TYPE_CONVERTER_SF2_IZONE_TO_DLS2_REGION \
-  (ipatch_converter_sf2_izone_to_dls2_region_get_type ())
-#define IPATCH_TYPE_CONVERTER_DLS2_REGION_TO_SF2_IZONE \
-  (ipatch_converter_dls2_region_to_sf2_izone_get_type ())
-#define IPATCH_TYPE_CONVERTER_SF2_SAMPLE_TO_DLS2_SAMPLE \
-  (ipatch_converter_sf2_sample_to_dls2_sample_get_type ())
-#define IPATCH_TYPE_CONVERTER_DLS2_SAMPLE_TO_SF2_SAMPLE \
-  (ipatch_converter_dls2_sample_to_sf2_sample_get_type ())
-#endif
-
-
 GType ipatch_converter_sf2_to_file_get_type (void);
 GType ipatch_converter_file_to_sf2_get_type (void);
 GType ipatch_converter_file_to_sf2_sample_get_type (void);
-
-#if 0
-GType ipatch_converter_sf2_to_dls2_get_type (void);
-GType ipatch_converter_dls2_to_sf2_get_type (void);
-GType ipatch_converter_sf2_preset_to_dls2_inst_get_type (void);
-GType ipatch_converter_dls2_inst_to_sf2_preset_get_type (void);
-GType ipatch_converter_sf2_inst_to_dls2_inst_get_type (void);
-GType ipatch_converter_dls2_inst_to_sf2_inst_get_type (void);
-GType ipatch_converter_sf2_pzone_to_dls2_region_get_type (void);
-GType ipatch_converter_dls2_region_to_sf2_pzone_get_type (void);
-GType ipatch_converter_sf2_izone_to_dls2_region_get_type (void);
-GType ipatch_converter_dls2_region_to_sf2_izone_get_type (void);
-GType ipatch_converter_sf2_sample_to_dls2_sample_get_type (void);
-GType ipatch_converter_dls2_sample_to_sf2_sample_get_type (void);
-#endif
 
 #endif
 
