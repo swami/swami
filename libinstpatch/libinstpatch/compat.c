9e6cbf4dfa7218339b313b526de3f032ce009054
Element Green <element@elementsofsound.org>
2014-04-25 18:50:17 -0600

    Forgot to commit the new compat.[ch] files for older glib compatibility stuff (previously committed).

---
+++ libinstpatch/libinstpatch/compat.c
@@ -0,0 +1,69 @@
+/*
+ * libInstPatch
+ * Copyright (C) 1999-2014 Element Green <element@elementsofsound.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1
+ * of the License only.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA or on the web at http://www.gnu.org.
+ */
+#include "compat.h"
+
+#ifdef IPATCH_COMPAT_GWEAKREF
+
+/* Older glib implementation of GWeakRef (not thread safe!) */
+
+void
+g_weak_ref_init (GWeakRef *weak_ref, gpointer object)
+{
+  weak_ref->obj = object;
+  if (object) g_object_add_weak_pointer ((GObject *)object, &weak_ref->obj);
+}
+
+void
+g_weak_ref_clear (GWeakRef *weak_ref)
+{
+  GObject *object;
+
+  object = weak_ref->obj;
+
+  if (object)
+  {
+    g_object_ref (object);      // ++ ref
+    g_object_remove_weak_pointer ((GObject *)object, &weak_ref->obj);
+    g_object_unref (object);    // -- unref
+    weak_ref->obj = NULL;
+  }
+}
+
+gpointer
+g_weak_ref_get (GWeakRef *weak_ref)
+{
+  GObject *object;
+
+  object = weak_ref->obj;
+
+  if (object) g_object_ref (object);      // ++ ref
+
+  return (object);              // !! caller takes over reference
+}
+
+void
+g_weak_ref_set (GWeakRef *weak_ref, gpointer object)
+{
+  g_weak_ref_clear (weak_ref);
+  g_weak_ref_init (weak_ref, object);
+}
+
+#endif
+
