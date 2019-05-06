swami/swami

commit d2574fc2f4d005c7b2cb844b1013c222ceb17745

-Add time.h header for Windows. -fix invalid arguments for macro.

src/libswami/SwamiLog.h
@@ -70,10 +70,10 @@ extern void _swami_pretty_log_handler (GLogLevelFlags flags,
 	 	 	     __FILE__, __PRETTY_FUNCTION__, __LINE__, \
 	 	 	     format, ## args);
 #else
-#define SWAMI_DEBUG(format, args...)
+#define SWAMI_DEBUG(format, args)
 #endif
 
-#define SWAMI_INFO(format, args...) \
+#define SWAMI_INFO(format, args) \
   _swami_pretty_log_handler (G_LOG_LEVEL_INFO, \
 	 	 	     __FILE__, __PRETTY_FUNCTION__, __LINE__, \
 	 	 	     format, ## args);
@@ -84,7 +84,7 @@ extern void _swami_pretty_log_handler (GLogLevelFlags flags,
 	 	 	     "Invalid function parameter value for '%s'.", \
 	 	 	     param);
 
-#define SWAMI_CRITICAL(format, args...) \
+#define SWAMI_CRITICAL(format, args) \
   _swami_pretty_log_handler (G_LOG_LEVEL_CRITICAL, \
 	 	 	     __FILE__, __PRETTY_FUNCTION__, __LINE__, \
 	 	 	     format, ## args);

src/libswami/SwamiControl.h
@@ -25,7 +25,7 @@
 typedef struct _SwamiControl SwamiControl;
 typedef struct _SwamiControlClass SwamiControlClass;
 
-#include <sys/time.h>
+//#include <sys/time.h>
 #include <glib.h>
 #include <glib-object.h>
 #include <libswami/SwamiLock.h>

src/libswami/SwamiControlEvent.h
@@ -23,7 +23,11 @@
 #ifndef __SWAMI_CONTROL_EVENT_H__
 #define __SWAMI_CONTROL_EVENT_H__
 
+#ifdef _WIN32
+#include<Windows.h>
+#else
 #include <sys/time.h>
+#endif
 #include <glib.h>
 
 #include <libinstpatch/IpatchUnit.h>

commit 8d0a2c28c5e3d7dd38c5cad91b669359831625d1

Fix incorrect change introduced by d2574fc

src/libswami/SwamiLog.h
@@ -71,10 +71,10 @@ extern void _swami_pretty_log_handler (GLogLevelFlags flags,
 	 	 	     __FILE__, __PRETTY_FUNCTION__, __LINE__, \
 	 	 	     format, ## args);
 #else
-#define SWAMI_DEBUG(format, args)
+#define SWAMI_DEBUG(format, args...)
 #endif
 
-#define SWAMI_INFO(format, args) \
+#define SWAMI_INFO(format, args...) \
   _swami_pretty_log_handler (G_LOG_LEVEL_INFO, \
 	 	 	     __FILE__, __PRETTY_FUNCTION__, __LINE__, \
 	 	 	     format, ## args);
@@ -85,7 +85,7 @@ extern void _swami_pretty_log_handler (GLogLevelFlags flags,
 	 	 	     "Invalid function parameter value for '%s'.", \
 	 	 	     param);
 
-#define SWAMI_CRITICAL(format, args) \
+#define SWAMI_CRITICAL(format, args...) \
   _swami_pretty_log_handler (G_LOG_LEVEL_CRITICAL, \
 	 	 	     __FILE__, __PRETTY_FUNCTION__, __LINE__, \
 	 	 	     format, ## args);