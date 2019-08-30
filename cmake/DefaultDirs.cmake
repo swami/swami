ec4fb46103f278046f0eb08bbc72ea659b1a2303
Dan Church <h3xx@users.noreply.github.com>
2019-08-30 11:12:25 -0500

    Use common vars for install directories (#9)
    This will make sure everything is installed in system-standardized locations. And of course, each one of the `CMAKE_INSTALL_*` variables are override-able.

--- cmake/DefaultDirs.cmake
+++ cmake/DefaultDirs.cmake
@@ -1,6 +1,8 @@
 # Several directory names used by libInstPatch to install files
 # the variable names are similar to the KDE4 build system
 
+include ( GNUInstallDirs )
+
 # BUNDLE_INSTALL_DIR - Mac only: the directory for application bundles 
 set (BUNDLE_INSTALL_DIR "/Applications" CACHE STRING 
      "The install dir for application bundles")
@@ -11,35 +13,9 @@ set (FRAMEWORK_INSTALL_DIR "/Library/Frameworks" CACHE STRING
      "The install dir for framework bundles")
 mark_as_advanced (FRAMEWORK_INSTALL_DIR)
 
-# BIN_INSTALL_DIR - the directory where executables will be installed
-set (BIN_INSTALL_DIR "bin" CACHE STRING "The install dir for executables")
-mark_as_advanced (BIN_INSTALL_DIR)
-
-# SBIN_INSTALL_DIR - the directory where system executables will be installed
-set (SBIN_INSTALL_DIR "sbin" CACHE STRING "The install dir for system executables")
-mark_as_advanced (SBIN_INSTALL_DIR)
-
-# LIB_INSTALL_DIR - the directory where libraries will be installed
-set (LIB_INSTALL_DIR "lib" CACHE STRING "The install dir for libraries")
-mark_as_advanced (LIB_INSTALL_DIR)
-
-# INCLUDE_INSTALL_DIR - the install dir for header files
-set (INCLUDE_INSTALL_DIR "include" CACHE STRING "The install dir for headers")
-mark_as_advanced (INCLUDE_INSTALL_DIR)
-
-# DATA_INSTALL_DIR - the base install directory for data files
-set (DATA_INSTALL_DIR "share" CACHE STRING 
-     "The base install dir for data files")
-mark_as_advanced (DATA_INSTALL_DIR)
-
-# DOC_INSTALL_DIR - the install dir for documentation
-set (DOC_INSTALL_DIR "share/doc" CACHE STRING 
-     "The install dir for documentation")
-mark_as_advanced (DOC_INSTALL_DIR)
-
 # PLUGINS_DIR - the directory where application plugins will be installed
 if ( UNIX )
-set (PLUGINS_DIR "${CMAKE_INSTALL_PREFIX}/lib/swami" CACHE STRING "The install dir for plugins")
+set (PLUGINS_DIR "${CMAKE_INSTALL_LIBDIR}/swami" CACHE STRING "The install dir for plugins")
 else ( UNIX )
 set (PLUGINS_DIR "plugins" CACHE STRING "The install dir for plugins")
 endif ( UNIX )
@@ -47,7 +23,7 @@ mark_as_advanced (PLUGINS_DIR)
 
 # IMAGES_DIR - the directory where images are installed
 if ( UNIX )
-set (IMAGES_DIR "${CMAKE_INSTALL_PREFIX}/share/swami/images" CACHE STRING "The install dir for images")
+set (IMAGES_DIR "${CMAKE_INSTALL_DATADIR}/swami/images" CACHE STRING "The install dir for images")
 else ( UNIX )
 set (IMAGES_DIR "images" CACHE STRING "The install dir for images")
 endif ( UNIX )
@@ -55,7 +31,7 @@ mark_as_advanced (IMAGES_DIR)
 
 # UIXML_DIR - the directory where UI XML is installed
 if ( UNIX )
-set (UIXML_DIR "${CMAKE_INSTALL_PREFIX}/share/swami" CACHE STRING "The install dir for UI XML files")
+set (UIXML_DIR "${CMAKE_INSTALL_DATADIR}/swami" CACHE STRING "The install dir for UI XML files")
 else ( UNIX )
 set (UIXML_DIR "." CACHE STRING "The install dir for UI XML files")
 endif ( UNIX )
