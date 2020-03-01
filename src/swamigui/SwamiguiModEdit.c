0cbb556b8a495898585069910959d7c77e4e4f7a
jjceresa <jjc_fluid@orange.fr>
2020-03-01 13:12:13 +0100

    This fix avoid the user to be forced to clic two time when he want to select another panel. The issue occurs when the user does the following actions: 1) select one panel (clic on notebook tab). 2) click inside the panel (not on child widget). 3) select another panel (clic on another notebook tab).    To get this other panel selected, the user need to click twice.

--- src/swamigui/SwamiguiModEdit.c
+++ src/swamigui/SwamiguiModEdit.c
@@ -357,10 +357,26 @@ swamigui_mod_edit_get_type (void)
   return (obj_type);
 }
 
+/* Override mouse button event:
+   To avoid lose of focus in pannels selector (GtkNoteBook tabs selector).
+   Otherwise,the user would be forced to clic two times when he want to select
+   another pannel.
+*/
+static gboolean swamigui_mod_edit_press_button (GtkWidget *widget,
+                                                GdkEventButton *event)
+{
+    /*  mouse click button propagation is ignored */
+    return TRUE;
+}
+
 static void
 swamigui_mod_edit_class_init (SwamiguiModEditClass *klass)
 {
   GObjectClass *obj_class = G_OBJECT_CLASS (klass);
+
+  /* Override mouse button event */
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+  widget_class->button_press_event = swamigui_mod_edit_press_button ;
 
   parent_class = g_type_class_peek_parent (klass);
 
